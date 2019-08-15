f69f7415e934277daf4d8447a0061a810116d0dc
Warren Loy Harper II <warrenharper2@att.net>
2019-08-15 20:45:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,17 @@
 def reverse_each_word(string)
-  array = string.split(" ") #turn string into an array
+  array = string.split(" ")      #turn string into an array
   nu_array = []
-  array.each do|string| #iterate over the array
-    nu_array << string.reverse #reverse each word in the array
+  array.each do|string|          #iterate over the array
+    nu_array << string.reverse   #reverse each word in the array
   end
   nu_array.join(" ")
 end
 
+def reverse_each_word(string)
+  array = string.split(" ")       #turn string into an array
+  nu_array = []
+  array.each do|string|           #iterate over the array
+    nu_array << string.reverse    #reverse each word in the array
+  end
+  nu_array.join(" ")
+end
